358d3220332361e237fd2e9ae4edfe6de030f821
Bas van Kampen <bas@ping6.nl>
2021-12-28 15:40:18 +0100

    Readme update

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ cd ~
 sudo apt install xclip vim kitty python3-pip
 
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/JetBrainsMono.zip
-unzip JetBrainsMono.zip JetBrains\ Mono\ Regular\ Nerd\ Font\ Complete.ttf -d .local/share/font
+unzip JetBrainsMono.zip JetBrains\ Mono\ Regular\ Nerd\ Font\ Complete.ttf -d .local/share/fonts
 fc-cache -f
 rm JetBrainsMono.zip
 
