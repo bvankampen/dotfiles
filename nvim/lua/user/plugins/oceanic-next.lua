de3cf3a56196c97e8eba1ab9079a261bae227d7f
Bas van Kampen <bas.vankampen@suse.com>
2022-10-14 10:58:37 +0200

    transparent background vim

--- nvim/lua/user/plugins/oceanic-next.lua
+++ nvim/lua/user/plugins/oceanic-next.lua
@@ -4,4 +4,10 @@ vim.cmd([[
   endif
 
   colorscheme OceanicNext
+
+  hi Normal guibg=NONE ctermbg=NONE
+  hi LineNr guibg=NONE ctermbg=NONE
+  hi SignColumn guibg=NONE ctermbg=NONE
+  hi EndOfBuffer guibg=NONE ctermbg=NONE
+
 ]])
