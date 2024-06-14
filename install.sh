577e07fe1bc92285c8638abcd660e49e9ae3993c
Bas van Kampen <bas@ping6.nl>
2024-06-14 11:23:45 +0200

    changed to ohmyposh, removed powerline and ohmyzh

--- install.sh
+++ install.sh
@@ -14,17 +14,6 @@ fi
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 ln -sf $HOME/.dotfiles/zshrc $HOME/.zshrc
-ln -sf $HOME/.dotfiles/my.zsh-theme $HOME/.oh-my-zsh/custom/themes/my.zsh-theme
-
-# tmux config
-pip install powerline-status
-ln -sf $HOME/.dotfiles/tmux.conf $HOME/.tmux.conf
-
-# Add current Kubernetes cluster to powerline status
-if command -v kubectl &> /dev/null; then
-  pip install powerk8s
-  ln -sf $HOME/.dotfiles/powerline $HOME/.config/powerline
-fi
 
 #astronvim
 ln -s ~/.dotfiles/astronvim/ ~/.config/nvim
@@ -42,13 +31,15 @@ cargo install lsd
 
 if [[ $(uname) == "Linux" ]]; then
   sudo zypper install lazygit
+  curl -s https://ohmyposh.dev/install.sh | bash -s
 fi
 if [[ $(uname) == "Darwin "]]; then
   brew install lazygit
+  brew install jandedobbeleer/oh-my-posh/oh-my-posh
 fi
 
 # powerline10k
 # https://github.com/romkatv/powerlevel10
 
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-ln -sf $HOME/.dotfiles/p10k.zsh $HOME/.p10k.zsh
+# git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+# ln -sf $HOME/.dotfiles/p10k.zsh $HOME/.p10k.zsh
